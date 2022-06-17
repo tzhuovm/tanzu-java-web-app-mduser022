3c1e13f4a38776c8636207ed6497da3a7c033582
tzhuovm <107604835+tzhuovm@users.noreply.github.com>
2022-06-17 00:24:46 -0700

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! + mduser022";
+		return "Greetings from Spring Boot + Tanzu! + mduser022test";
 	}
 
 }
